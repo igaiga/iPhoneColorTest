308f06d45e3f15c73f8f74f9b2f82af705dece80
Kuniaki IGARASHI <igaiga@gmail.com>
2011-08-19 19:50:46 +0900

    Initial Commit

---
+++ colortest2/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  colortest2
+//
+//  Created by Kuniaki IGARASHI on 11/08/19.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ colortest2/colortest2AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  colortest2AppDelegate.h
+//  colortest2
+//
+//  Created by Kuniaki IGARASHI on 11/08/19.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface colortest2AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
